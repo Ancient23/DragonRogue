52ad23f16664adfde64dd8b4d49562f48db004db
Filip Iliescu <Ancient23@users.noreply.github.com>
2024-07-11 14:01:22 -0700

    Six projectile damaging instigator

--- Source/DragonRogue/Private/DaMagicProjectile.cpp
+++ Source/DragonRogue/Private/DaMagicProjectile.cpp
@@ -17,7 +17,7 @@ ADaMagicProjectile::ADaMagicProjectile()
 
 void ADaMagicProjectile::OnActorOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult)
 {
-	if (OtherActor)
+	if (OtherActor && OtherActor != GetInstigator())
 	{
 		UDaAttributeComponent* AttributeComp = Cast<UDaAttributeComponent>(OtherActor->GetComponentByClass(UDaAttributeComponent::StaticClass()));
 		if (AttributeComp)
