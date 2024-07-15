01e6995f27b8db37cde904782e7eb9edcbe81ff8
Filip Iliescu <Ancient23@users.noreply.github.com>
2024-07-15 16:33:56 -0700

    Stub AI Character in

---
+++ Source/DragonRogue/Public/AI/DaAIController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "DaAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DRAGONROGUE_API ADaAIController : public AAIController
+{
+	GENERATED_BODY()
+	
+};
