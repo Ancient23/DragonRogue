bdfa9dd022075f3f66eade3920634b282b051aff
Filip Iliescu <Ancient23@users.noreply.github.com>
2024-07-15 22:59:34 -0700

    AI Character Follows and shoot in bursts

---
+++ Source/DragonRogue/Private/AI/DaBTTask_RangedAttack.cpp
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/DaBTTask_RangedAttack.h"
+
+#include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "GameFramework/Character.h"
+
+EBTNodeResult::Type UDaBTTask_RangedAttack::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	AAIController* MyController = OwnerComp.GetAIOwner();
+	if (ensure(MyController))
+	{
+		ACharacter* Character = Cast<ACharacter>(MyController->GetPawn());
+		if (Character == nullptr)
+		{
+			return EBTNodeResult::Failed;
+		}
+
+		FVector MuzzleLocation = Character->GetMesh()->GetSocketLocation("Muzzle_Front");
+
+		AActor* TargetActor = Cast<AActor>(OwnerComp.GetBlackboardComponent()->GetValueAsObject("TargetActor"));
+		if (TargetActor == nullptr)
+		{
+			return EBTNodeResult::Failed;
+		}
+
+		FVector Direction = TargetActor->GetActorLocation() - MuzzleLocation;
+		FRotator MuzzleRotation = Direction.Rotation();
+
+		FActorSpawnParameters SpawnParams;
+		SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+		SpawnParams.Instigator = Character;
+
+		AActor* NewProj = GetWorld()->SpawnActor<AActor>(ProjectileClass, MuzzleLocation, MuzzleRotation, SpawnParams);
+
+		return NewProj ? EBTNodeResult::Succeeded : EBTNodeResult::Failed;
+	}
+	
+	return EBTNodeResult::Failed;
+}
