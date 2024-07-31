893e09b2de12054c2b9ea92b49fe6e00a3f782ed
Filip Iliescu <Ancient23@users.noreply.github.com>
2024-07-30 19:01:01 -0700

    Fix a bug if Burning Effect is not set it would assert

--- Source/DragonRogue/Private/DaMagicProjectile.cpp
+++ Source/DragonRogue/Private/DaMagicProjectile.cpp
@@ -43,7 +43,7 @@ void ADaMagicProjectile::OnActorOverlap(UPrimitiveComponent* OverlappedComponent
 		{
 			Explode();
 
-			if (ActionComp)
+			if (ActionComp && BurningActionClass)
 			{
 				ActionComp->AddAction(OtherActor, BurningActionClass);
 			}
