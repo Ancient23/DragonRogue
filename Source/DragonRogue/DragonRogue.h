312b9465ec808dd4a3cd4003a8358318d92e184b
Filip Iliescu <Ancient23@users.noreply.github.com>
2024-08-05 16:25:41 -0700

    Add a static logging function for debugging multiplayer

--- Source/DragonRogue/DragonRogue.h
+++ Source/DragonRogue/DragonRogue.h
@@ -11,3 +11,24 @@ DECLARE_LOG_CATEGORY_EXTERN(DragonRogue, Log, All);
 #define LOG(x, ...) UE_LOG(DragonRogue, Log, TEXT(x), ##__VA_ARGS__)
 #define LOG_WARNING(x, ...) UE_LOG(DragonRogue, Warning, TEXT(x), ##__VA_ARGS__)
 #define LOG_ERROR(x, ...) UE_LOG(DragonRogue, Error, TEXT(x), ##__VA_ARGS__)
+
+static void LogOnScreen(UObject* WorldContext, FString Msg, FColor Color = FColor::White, float Duration = 5.0f)
+{
+	if (!ensure(WorldContext))
+	{
+		return;
+	}
+
+	UWorld* World = WorldContext->GetWorld();
+	if (!ensure(World))
+	{
+		return;
+	}
+
+	FString NetPrefix = World->IsNetMode(NM_Client) ? "[CLIENT] " : "[SERVER] ";
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, Duration, Color, NetPrefix + Msg);
+	}
+}
+
