88a7ca7048706f6548ff1a73ff082007848271f7
Filip Iliescu <Ancient23@users.noreply.github.com>
2024-08-18 00:57:43 -0700

    After profiling, were leaking magic projectiles. Add InitialLifeSpan 0f 10 sec

--- Source/DragonRogue/Private/DaMagicProjectile.cpp
+++ Source/DragonRogue/Private/DaMagicProjectile.cpp
@@ -22,6 +22,8 @@ ADaMagicProjectile::ADaMagicProjectile()
 	PrimaryHandSocketName = "Muzzle_01";
 	SecondaryHandSocketName = "Muzzle_02";
 	EyeSocketName = "Eyes_Position";
+
+	InitialLifeSpan = 10.0f;
 }
 
 void ADaMagicProjectile::OnActorOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult)
